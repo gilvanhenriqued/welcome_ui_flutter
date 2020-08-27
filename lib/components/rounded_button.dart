c63ee6d1497c99e40ed589d8ec147228a42d01fc
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-27 09:37:16 -0300

    rounded button component

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:welcome_ui_flutter/constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function pressFunction;
+  final Color color, textColor;
+
+  const RoundedButton({
+    Key key,
+    this.text,
+    this.pressFunction,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  
+    Size sizeScreen = MediaQuery.of(context).size;
+
+    return Container(
+      width: sizeScreen.width * 0.7,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          color: color,
+          onPressed: pressFunction,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/Welcome/components/body.dart
+++ lib/pages/Welcome/components/body.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:welcome_ui_flutter/components/rounded_button.dart';
+import 'package:welcome_ui_flutter/constants.dart';
 import 'package:welcome_ui_flutter/pages/Welcome/components/background.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Size sizeScreen = MediaQuery.of(context).size;
+
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -13,7 +19,23 @@ class Body extends StatelessWidget {
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
-          )
+          ),
+          SvgPicture.asset(
+            'assets/icons/chat.svg',
+            height: sizeScreen.height * 0.45,
+          ),
+          RoundedButton(
+            text: "LOGIN",
+            pressFunction: (){
+
+            },
+          ),
+          RoundedButton(
+            text: "LOGIN",
+            pressFunction: (){
+
+            },
+          ),
         ],
       )
     );
